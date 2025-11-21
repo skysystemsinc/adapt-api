68bb2e141bd4cb923733fa782b892fcd7c043e32
skysystemsinc <developers@myskysys.com>
2025-11-21 20:32:24 +0500

    Feature: Get all HOD for assignment

--- src/modules/warehouse-admin/warehouse-admin.module.ts
+++ src/modules/warehouse-admin/warehouse-admin.module.ts
@@ -3,10 +3,11 @@ import { WarehouseAdminService } from './warehouse-admin.service';
 import { WarehouseAdminController } from './warehouse-admin.controller';
 import { WarehouseOperatorApplicationRequest } from '../warehouse/entities/warehouse-operator-application-request.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   controllers: [WarehouseAdminController],
   providers: [WarehouseAdminService],
-  imports: [TypeOrmModule.forFeature([WarehouseOperatorApplicationRequest])],
+  imports: [TypeOrmModule.forFeature([WarehouseOperatorApplicationRequest, User])],
 })
 export class WarehouseAdminModule {}
