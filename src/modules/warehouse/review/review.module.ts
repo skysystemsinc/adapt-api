4567deb71604677857636bebdbf6aa1d4679bbf9
skysystemsinc <developers@myskysys.com>
2025-12-04 00:53:43 +0500

    Added InspectionReport dependcy in ReviewModule

--- src/modules/warehouse/review/review.module.ts
+++ src/modules/warehouse/review/review.module.ts
@@ -4,10 +4,11 @@ import { ReviewService } from './review.service';
 import { ReviewController } from './review.controller';
 import { ReviewEntity } from './entities/review.entity';
 import { AssessmentDetailsEntity } from './entities/assessment_details.entity';
+import { InspectionReport } from '../../inspection-reports/entities/inspection-report.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ReviewEntity, AssessmentDetailsEntity]),
+    TypeOrmModule.forFeature([ReviewEntity, AssessmentDetailsEntity, InspectionReport]),
   ],
   controllers: [ReviewController],
   providers: [ReviewService],
