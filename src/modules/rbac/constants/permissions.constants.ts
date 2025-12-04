679a5cbbb2ee08232d3550cf4c52e79fff6f74fb
subhan-skysys <muhammad.subhan@myskysys.com>
2025-12-04 18:03:46 +0500

    permission updated

--- src/modules/rbac/constants/permissions.constants.ts
+++ src/modules/rbac/constants/permissions.constants.ts
@@ -7,35 +7,35 @@ export enum Permissions {
 
   // Warehouse operator application permissions
   CREATE_OPERATOR_APPLICATION = 'create_operator_application',
-  
+
   // Report permissions
   VIEW_REPORT = 'view_report',
   EXPORT_REPORT = 'export_report',
-  
+
   // Admin permissions
   MANAGE_RBAC = 'manage_rbac',
   MANAGE_ROLES = 'manage_roles',
   MANAGE_PERMISSIONS = 'manage_permissions',
   ASSIGN_ROLES = 'assign_roles',
-  
+
   // User management
   VIEW_USERS = 'view_users',
   CREATE_USERS = 'create_users',
   UPDATE_USERS = 'update_users',
   DELETE_USERS = 'delete_users',
-  
+
   // Registration application admin
   VIEW_ALL_APPLICATIONS = 'view_all_applications',
   APPROVE_APPLICATION = 'approve_application',
   REJECT_APPLICATION = 'reject_application',
   UPDATE_APPLICATION_STATUS = 'update_application_status',
-  
+
   // KYC verification
   VERIFY_KYC = 'verify_kyc',
   REVIEW_KYC = 'review_kyc',
   VIEW_KYC = 'view_kyc',
   REVIEW_KYC_APPROVAL = 'review_kyc_approval',
-  
+
   // Finance
   VIEW_FINANCE_REPORTS = 'view_finance_reports',
   APPROVE_FINANCE = 'approve_finance',
@@ -75,5 +75,13 @@ export enum Permissions {
   // Settings Tab Permissions
   VIEW_SETTINGS = 'view_settings',
   MANAGE_SETTINGS = 'manage_settings',
+
+  // Organization permissions
+  VIEW_ORGANIZATION = 'view_organization',
+  MANAGE_ORGANIZATION = 'manage_organization',
+
+  // Document Type permissions
+  VIEW_DOCUMENT_TYPE = 'view_document_type',
+  MANAGE_DOCUMENT_TYPE = 'manage_document_type',
 }
 
