a3cf4701ec9d70a4cfb5d8575ca44c18d2579ae2
skysys-hassankhan <hassan.khan@myskysys.com>
2025-12-10 23:45:52 +0500

    Add DynamicCalculatorController for public access to configurations
    - Created DynamicCalculatorController to handle requests for retrieving all dynamic calculator configurations.
    - Integrated Swagger API documentation for the controller's endpoint.
    - Updated DynamicCalculatorModule to include the new controller alongside the existing admin controller.

--- src/modules/dynamic-calculator/dynamic-calculator.module.ts
+++ src/modules/dynamic-calculator/dynamic-calculator.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DynamicCalculatorService } from './dynamic-calculator.service';
 import { DynamicCalculatorAdminController } from './dynamic-calculator-admin.controller';
+import { DynamicCalculatorController } from './dynamic-calculator.controller';
 import { DynamicCalculator } from './entities/dynamic-calculator.entity';
 import { Setting } from '../settings/entities/setting.entity';
 import { RBACModule } from '../rbac/rbac.module';
@@ -21,7 +22,7 @@ import { JwtModule } from '@nestjs/jwt';
       inject: [ConfigService],
     }),
   ],
-  controllers: [DynamicCalculatorAdminController],
+  controllers: [DynamicCalculatorController, DynamicCalculatorAdminController],
   providers: [DynamicCalculatorService],
   exports: [DynamicCalculatorService],
 })
