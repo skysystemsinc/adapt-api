0050262f436a4929d13911fce8d7d50433783260
subhan-skysys <muhammad.subhan@myskysys.com>
2025-12-13 06:11:20 +0500

    waerhouse module entity added

--- src/modules/warehouse/warehouse.module.ts
+++ src/modules/warehouse/warehouse.module.ts
@@ -42,6 +42,8 @@ import { CompanyInformationHistory } from './entities/company-information-histor
 import { AcademicQualificationsHistoryEntity } from './entities/hr/academic-qualifications.entity/academic-qualifications-history.entity';
 import { PersonalDetailsHistoryEntity } from './entities/hr/personal-details.entity/personal-details-history.entity';
 import { ProfessionalQualificationsHistoryEntity } from './entities/hr/professional-qualifications.entity/professional-qualifications-history.entity';
+import { TrainingsHistoryEntity } from './entities/hr/trainings.entity/trainings-history.entity';
+import { DeclarationHistoryEntity } from './entities/hr/declaration.entity/declaration-history.entity';
 
 @Module({
   controllers: [WarehouseController],
@@ -81,6 +83,11 @@ import { ProfessionalQualificationsHistoryEntity } from './entities/hr/professio
       AcademicQualificationsHistoryEntity,
       PersonalDetailsHistoryEntity,
       ProfessionalQualificationsHistoryEntity,
+      PersonalDetailsHistoryEntity,
+      ProfessionalQualificationsHistoryEntity,
+      DeclarationHistoryEntity,
+      TrainingsHistoryEntity,
+      DeclarationHistoryEntity,
     ]),
     forwardRef(() => AuthModule),
     JwtModule.registerAsync({
